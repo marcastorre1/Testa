fe67f75c131cf4593c6638ece1f18e70d331de02
marcastorre1 <gygygaga645@gmail.com>
2026-09-16 00:01:15 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE = 'nova-v1';
+const FILES = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/android-chrome-192x192.png',
+  '/android-chrome-512x512.png'
+];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES).catch(()=>{}))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if(e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const network = fetch(e.request).then(res => {
+        if(res && res.status === 200 && res.type === 'basic'){
+          const copy = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, copy));
+        }
+        return res;
+      }).catch(() => cached);
+      return cached || network;
+    })
+  );
+});
